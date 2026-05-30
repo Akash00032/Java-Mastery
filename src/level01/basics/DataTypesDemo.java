c8ebcebba66c134cc54dff11f46c4b5b36ad11f5
Akash Kumar <aakashkumar572@gmail.com>
2026-05-30 12:05:51 +0530

    Added VariablesDemo and DataTypesDemo

---
+++ src/level01/basics/DataTypesDemo.java
@@ -0,0 +1,27 @@
+package level01.basics;
+
+public class DataTypesDemo {
+    public static void main(String[] args) {
+
+        String companyName = "Infosys";
+        byte experience = 1;
+        short passoutYear = 2026;
+        int salary = 55000;
+        long mobileNumber = 9910360596L;
+        float percentage = 69.5f;
+        double cgpa = 6.90;
+        char gender = 'M';
+        boolean isPlaced = true;
+
+
+        System.out.println("Company Name : " + companyName);
+        System.out.println("Experience   : " + experience);
+        System.out.println("Passout Year : " + passoutYear);
+        System.out.println("Salary       : " + salary);
+        System.out.println("Mobile No.   : " + mobileNumber);
+        System.out.println("Percentage   : " + percentage);
+        System.out.println("CGPA         : " + cgpa);
+        System.out.println("Gender       : " + gender);
+        System.out.println("Placed       : " + isPlaced);
+    }
+}
